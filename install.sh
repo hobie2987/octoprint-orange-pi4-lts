0f83050a8198d53b52433683427e9e6b78530513
hobie2987 <hobie2987@gmail.com>
2022-05-04 07:43:04 -0400

    update scripts

--- install.sh
+++ install.sh
@@ -9,13 +9,9 @@ function sys_update() {
 }
 
 function pi_user() {
-  echo 'Creating sudo user: pi...'
-  sudo adduser pi
-
   if ! id -u 'pi' >/dev/null 2>&1; then
-    echo 'User pi failed to be created...'
-    exit -1
-  else
+    echo 'Creating sudo user: pi...'
+    sudo adduser pi
     echo 'Granting pi user sudo permissions...'
     sudo usermod -aG sudo pi
     sudo usermod -a -G tty pi
@@ -28,8 +24,10 @@ function install_octoprint() {
   python3 --version
   sudo apt update
   sudo apt install python3-pip python3-dev python3-setuptools python3-venv git libyaml-dev build-essential
-  cd ~
-  mkdir OctoPrint && cd OctoPrint
+  cd /home/pi
+  mkdir OctoPrint
+  cd OctoPrint
+  pwd
   python3 -m venv venv
   source venv/bin/activate
   pip install pip --upgrade
@@ -42,42 +40,50 @@ function install_octoprint() {
 }
 
 function webcam_support() {
-  cd ~
+  cd /home/pi
   echo 'Installing mjpg-streamer...'
   sudo apt install subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
   git clone https://github.com/jacksonliam/mjpg-streamer.git
   cd mjpg-streamer/mjpg-streamer-experimental
   export LD_LIBRARY_PATH=.
   make
-  cd ~
-  mkdir scripts && cd scripts
+  cd /home/pi
+  mkdir scripts
+  cd scripts
+  pwd
 # TODO enable reading in of resolution and updating webcam daemon script
 # read -p "Would resolution is your webcam? [1920x1080]" -i Y resolution
 # read -p "Would is the frame rate of your webcam? [30]" -i Y frame_rate
   echo 'Install webcam auto-start scripts...'
-  wget https://raw.githubusercontent.com/hobie2987/octoprint-orange-pi4-lts/main/scripts/webcamd && sudo mv webcamd /home/pi/scripts/webcamd
+  wget https://raw.githubusercontent.com/hobie2987/octoprint-orange-pi4-lts/main/scripts/webcamd
   echo 'Updating script permissions...'
-  chmod +x /home/pi/scripts/webcamd
+  sudo chmod +x /home/pi/scripts/webcamd
   echo 'Install webcam services...'
   wget https://raw.githubusercontent.com/hobie2987/octoprint-orange-pi4-lts/main/services/webcamd.service && sudo mv webcamd.service /etc/systemd/system/webcamd.service
+  sudo systemctl daemon-reload
   sudo systemctl enable webcamd.service
+  sudo systemctl start webcamd
   echo 'Webcam services installed!...'
 }
 
 function set_permissions() {
-  sudo su root
-  read -n 1 -p "Would you like to disable password for user pi? [y,n]: " -i Y nopass
-  if [ "$nopass" == "y" ]; then
+  echo 'Lets set sme permissions for user pi...'
+  #read -n 1 -p "Would you like to disable password for user pi? [y,n]: " -i Y nopass
+  #if [ "$nopass" == "y" ]; then
     echo 'pi ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
-  else
+  #else
     echo 'pi ALL=NOPASSWD: /sbin/shutdown' | sudo tee -a /etc/sudoers.d/octoprint-shutdown
     echo 'pi ALL=NOPASSWD: /usr/sbin/service' | sudo tee -a /etc/sudoers.d/octoprint-service
-  fi
+  #fi
 }
 
 function fin() {
   echo 'OctoPrint is now installed with webcam support!  Please reboot your Orange Pi!'
-  exit 0
+  echo '-------------------------------------------------------------------------------'
+  hostname = $(hostname -I)
+  echo 'OctoPrint is running @: ${hostname}:5000';
+  echo 'Webcam stream is running @: ${hostname}:8080?action=stream';
+  echo '-------------------------------------------------------------------------------'
 }
 
 sys_update
@@ -86,3 +92,4 @@ install_octoprint
 webcam_support
 set_permissions
 fin
+exit 0
