b2a7290bce06e7c7b2c95a143e41ccf8a70bc12b
hobie2987 <hobie2987@gmail.com>
2023-06-14 16:54:27 -0400

    chore(install): consolidate common tasks

--- install.sh
+++ install.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 #set -x #echo on
 
-hostname=$(hostname -I | cut -d" " -f1)
+HOSTNAME=$(hostname -I | cut -d" " -f1)
+PI_HOME="/home/pi"
+OCTOPRINT_DIR="$PI_HOME/OctoPrint"
+MJPG_STREAMER_DIR="$PI_HOME/mjpg-streamer"
+SCRIPTS_DIR="$PI_HOME/scripts"
+WEBCAM_DAEMON="$SCRIPTS_DIR/webcamd"
+TEMP_DIR="$PI_PI_HOME/tmp"
 
-# Green text
+# Magenta text
 function INFO() {
   echo -e "\e[1;35m$1\e[1;m"
-#  echo $1
 }
 
 # Yellow text
 function WARN() {
   echo -e "\e[1;33m$1\e[1;m"
-  #  echo $1
+}
+
+# Red text
+function ERROR() {
+  echo -e "\e[1;30m$1\e[1;m"
 }
 
 # Run system Updates/Upgrades
@@ -23,90 +32,75 @@ function sys_update() {
 }
 
 # Creates user "pi",
-# Assign pi user to sudo group
+# Assign pi user to groups: sudo, video, tty, & dialout
+# Creates directories /home/pi/[OctoPrint,scripts,mjpg-streamer,tmp]
 function pi_user() {
   if ! id -u 'pi' >/dev/null 2>&1; then
     INFO 'Creating sudo user: pi...'
     sudo adduser pi
   else
-    WARN 'pi user exists, but permissions will be updated'
+    WARN 'User pi exists... updating user groups'
   fi
 
-  INFO 'Granting pi user sudo permissions...'
-  sudo usermod -a -G sudo pi
+  INFO 'Adding pi to necessary groups [sudo, video, tty, dialout]...'
+  sudo usermod -a -G sudo,video,tty,dialout pi
 
-  INFO 'Granting pi user video permissions...'
-  sudo usermod -a -G video pi
-
-  INFO 'Granting pi user serial port access...'
-  sudo usermod -a -G tty pi
-  sudo usermod -a -G dialout pi
+  INFO "Creating necessary directories..."
+  mkdir -v -p "$OCTOPRINT_DIR" "$SCRIPTS_DIR" "$MJPG_STREAMER_DIR" "$TEMP_DIR"
 }
 
-# Download Python dependencies
-# Make OctoPrint install directory
+# Installs required supporting packages
+function install_packages() {
+  INFO "Installing necessary packages..."
+  INFO "Python version: $(python3 --version)"
+  # Python3 + Virtual Environment Support
+  py_pkg="python3-pip python3-dev python3-setuptools python3-venv git libyaml-dev build-essential"
+  # Webcam support
+  # Removed - subversion
+  webcam_pkg="libjpeg-dev imagemagick ffmpeg libv4l-dev cmake v4l-utils"
+  # Reverse Proxy support
+  proxy_pkg="haproxy"
+
+  sudo apt install "$py_pkg $webcam_pkg $proxy_pkg"
+}
+
+# Downloads service scripts and moves them to their appropriate destinations
+function install_services() {
+  INFO 'Installing Auto-Start Services...'
+  cd "$TEMP_DIR"
+  INFO 'Installing OctoPrint auto-start scripts...'
+  wget https://github.com/OctoPrint/OctoPrint/raw/master/scripts/octoprint.service && sudo mv octoprint.service /etc/systemd/system/octoprint.service
+  INFO 'Installing Webcam auto-start services...'
+  wget https://raw.githubusercontent.com/hobie2987/octoprint-orange-pi4-lts/main/services/webcamd.service && sudo mv webcamd.service /etc/systemd/system/webcamd.service
+  INFO 'Installing Webcam auto-start scripts...'
+  wget https://raw.githubusercontent.com/hobie2987/octoprint-orange-pi4-lts/main/scripts/webcamd && sudo mv webcamd "$WEBCAM_DAEMON"
+  sudo chmod +x "$WEBCAM_DAEMON"
+}
+
 # Generate VM for OctoPrint, and install
-# Download Octoprint auto-start services
 function install_octoprint() {
-  INFO 'Installing Python virtual environment...'
-  python3 --version # Prints Python version
-  sudo apt update
-  sudo apt install python3-pip python3-dev python3-setuptools python3-venv git libyaml-dev build-essential
-  # sudo apt install libraspberrypi-bin ?????
-  cd /home/pi
-  mkdir -v OctoPrint
-  cd OctoPrint
-  pwd
+  INFO 'Installing OctoPrint in Python virtual environment...'
+  cd "$OCTOPRINT_DIR"
   python3 -m venv venv
   source venv/bin/activate
   pip install pip --upgrade
-  INFO 'Installing OctoPrint in virtual environment...'
   pip install octoprint
-  INFO 'Installing OctoPrint auto-start scripts...'
-  wget https://github.com/OctoPrint/OctoPrint/raw/master/scripts/octoprint.service && sudo mv octoprint.service /etc/systemd/system/octoprint.service
-  cd /home/pi
-  sudo chown -R pi OctoPrint
-  INFO 'OctoPrint services installed!...'
+  sudo chown -R pi "$OCTOPRINT_DIR"
 }
 
 # Install MJPG dependencies
 # Clone Git repo
-# Download Webcam Daemon script, and add execute permissions
-# Provide video access to pi user
-# Download webcam services
 function webcam_support() {
-  cd /home/pi
   INFO 'Installing mjpg-streamer...'
-  #sudo apt install subversion libjpeg62-turbo-dev imagemagick ffmpeg libv4l-dev cmake
-  sudo apt install subversion libjpeg-dev imagemagick ffmpeg libv4l-dev cmake
-  git clone https://github.com/jacksonliam/mjpg-streamer.git
-  # sudo chown -R pi mjpg-streamer
-  cd mjpg-streamer/mjpg-streamer-experimental
+  git clone https://github.com/jacksonliam/mjpg-streamer.git "$MJPG_STREAMER_DIR"
+  cd "$MJPG_STREAMER_DIR/mjpg-streamer-experimental"
   export LD_LIBRARY_PATH=.
-  sudo make
-  cd /home/pi
-  mkdir -v scripts
-  cd scripts
-  pwd
-# TODO enable reading in of resolution and updating webcam daemon script
-# read -p "Would resolution is your webcam? [1920x1080]" -i Y resolution
-# read -p "Would is the frame rate of your webcam? [30]" -i Y frame_rate
-  INFO 'Installing webcam auto-start scripts...'
-  wget https://raw.githubusercontent.com/hobie2987/octoprint-orange-pi4-lts/main/scripts/webcamd
-  INFO 'Updating script permissions...'
-  sudo chmod +x /home/pi/scripts/webcamd
-  sudo chmod 666 /dev/video0
-  INFO 'Install webcam services...'
-  wget https://raw.githubusercontent.com/hobie2987/octoprint-orange-pi4-lts/main/services/webcamd.service && sudo mv webcamd.service /etc/systemd/system/webcamd.service
-  INFO 'Webcam services installed!...'
+  sudo make install
 }
 
 # Remove sudo password requirements for user pi
 function set_permissions() {
   INFO 'Removing password requirement for user pi...'
-#    echo 'pi ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
-#    echo 'pi ALL=NOPASSWD: /sbin/shutdown' | sudo tee -a /etc/sudoers.d/octoprint-shutdown
-#    echo 'pi ALL=NOPASSWD: /usr/sbin/service' | sudo tee -a /etc/sudoers.d/octoprint-service
   sudo tee -a /etc/sudoers > /dev/null <<EOT
 
 pi ALL=(ALL) NOPASSWD:ALL
@@ -123,9 +117,9 @@ pi ALL=NOPASSWD: /usr/sbin/service
 EOT
 }
 
+# Configure OctoPrint to be be accessible on port 80
 function reverse_proxy() {
   INFO 'Configuring reverse proxy...'
-  sudo apt install haproxy
   sudo tee -a /etc/haproxy/haproxy.cfg > /dev/null <<EOT
 
 frontend public
@@ -156,12 +150,16 @@ function start_services() {
   sudo systemctl start webcamd
 }
 
+function cleanup() {
+  rm -rf "$TEMP_DIR"
+}
+
 # Output URLs for OctoPrint and Webcam stream
 function fin() {
   INFO 'OctoPrint is now installed with webcam support!  Please reboot your system!'
   INFO '-------------------------------------------------------------------------------'
-  INFO 'OctoPrint is running @: http://'${hostname}/
-  INFO 'Webcam stream is running @: http://'${hostname}/webcam/?action=stream
+  INFO 'OctoPrint is running @: http://'${HOSTNAME}/
+  INFO 'Webcam stream is running @: http://'${HOSTNAME}/webcam/?action=stream
   INFO '-------------------------------------------------------------------------------'
   INFO 'System commands'
   INFO 'Restart OctoPrint: sudo service octoprint restart'
@@ -173,17 +171,26 @@ function fin() {
   INFO 'Snapshot URL: http://127.0.0.1:8080/?action=snapshot'
   INFO 'Path to FFMPEG: /usr/bin/ffmpeg'
   INFO '-------------------------------------------------------------------------------'
-  # serverRestartCommand - sudo service octoprint restart
-  # systemRestartCommand - sudo shutdown -r now
-  # systemShutdownCommand - sudo shutdown -h now
+
+  WARN "Please reboot your device for these changes to take effect..."
+  read -p "Press [R] to reboot, or any other key to exit: " -rsn1 input
+    if [ "$input" = "r" ]; then
+      WARN "Rebooting your device"
+      sudo shutdown now -r
+    else
+      WARN "Exiting"
+      exit 0
+    fi
 }
 
-sys_update
-pi_user
-install_octoprint
-webcam_support
-set_permissions
-reverse_proxy
-start_services
-fin
-exit 0
+sys_update # Run System update/upgrade
+install_packages # Install Supporting packages
+pi_user # Create pi user, add user groups, and create required install directories
+install_services # Download auto-start services and scripts
+install_octoprint # Create virtual env and install OctoPrint
+webcam_support # Clone MJPG streamer and set LD_LIBRARY_PATH
+set_permissions # Remove sudo password requirement for user PI
+reverse_proxy # Enable OctoPrint on port 80
+start_services # Add and restart Daemons
+cleanup # Delete temp directory
+fin # Output OctoPrint/Webcam URLs/config, prompt reboot
